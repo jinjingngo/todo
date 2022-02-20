a23af2ae04977c57c37975e76db0de57fd59936a
charleserious <wujinjing25@gmail.com>
2022-02-20 22:45:28 +0800

    fulfill TodoAdd

--- apps/react/src/TodoAdd/TodoAdd.tsx
+++ apps/react/src/TodoAdd/TodoAdd.tsx
@@ -1,13 +1,54 @@
-import React from 'react';
+import React, {useState, ChangeEvent, KeyboardEvent, MouseEvent} from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
-const TodoAdd = (): JSX.Element => {
+export interface TodoAddProps {
+  onAdd?: (text: string) => void;
+}
+
+const TodoAdd = (props: TodoAddProps): JSX.Element => {
+  const {onAdd} = props;
+  const [value, setValue] = useState('');
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    const {value = ''} = event.target;
+
+    setValue(value);
+  };
+
+  const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>): void => {
+    const {key} = event;
+
+    if (key !== 'Enter') return;
+
+    onAdd?.(value);
+    onAdd && setValue('');
+  };
+
+  const handleClick = (event: MouseEvent<HTMLButtonElement>): void => {
+    onAdd?.(value);
+    onAdd && setValue('');
+  };
+
   return (
     <Box sx={{display: 'flex', alignContent: 'space-around', justifyContent: 'space-between'}}>
-      <TextField label="Input todo item" variant="standard" />
-      <Button variant="text">Add</Button>
+      <TextField
+        label="Input todo item"
+        variant="standard"
+        sx={{width: '85%'}}
+        value={value}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+      />
+      <Button
+        size="large"
+        variant="text"
+        sx={{textTransform: 'unset', padding: '16px 11px 0px'}}
+        onClick={handleClick}
+      >
+        Add
+      </Button>
     </Box>
   );
 };
--- apps/react/src/entry.tsx
+++ apps/react/src/entry.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CssBaseline from '@mui/material/CssBaseline';
 
 import App from './App';
 import './index.pcss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CssBaseline>
+      <App />
+    </CssBaseline>
   </React.StrictMode>,
   document.querySelector('#root')
 );
