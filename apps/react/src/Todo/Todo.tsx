da0f5f3d6bdc245a8ed5b4a3a39d3b27ea5a4d92
charleserious <wujinjing25@gmail.com>
2022-02-21 10:44:35 +0800

    Feat: define Task interface 1. define `Task` interface 2. `TodoAdd` component `onAdd` prop paramater follow the `Task` interface

--- apps/react/src/Todo/Todo.tsx
+++ apps/react/src/Todo/Todo.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import TodoAdd from '../TodoAdd';
 
+export interface Task {
+  id?: number;
+  title: string;
+  completed: boolean;
+}
+
 const Todo = (): JSX.Element => {
   return (
     <>
